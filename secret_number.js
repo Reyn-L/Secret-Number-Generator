c8f49f7a985b043c577864c31bc228fffb775881
Reyn-L <Reyn.Leong@gmail.com>
2017-06-15 13:48:40 -1000

    Finished Revised

--- secret_number.js
+++ secret_number.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 'use strict';
 module.exports = function() {
-    let secretNumber = (Math.random()*1000000 + 1);
+    let secretNumber = (Math.random()*1000001);
       return function() {
         return secretNumber;
       };
