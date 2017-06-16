07a476dfce4bd0be0fc9b55097422d8df3e0f9ed
Reyn-L <Reyn.Leong@gmail.com>
2017-06-15 14:09:03 -1000

    Added math.floor

--- secret_number.js
+++ secret_number.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 'use strict';
 module.exports = function() {
-    let secretNumber = (Math.random()*1000001);
+    const secretNumber = Math.floor(Math.random()*1000001);
       return function() {
         return secretNumber;
       };
